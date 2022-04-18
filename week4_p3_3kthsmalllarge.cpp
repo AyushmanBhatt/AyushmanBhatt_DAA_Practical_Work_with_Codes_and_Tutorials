e1d5ad549f52baf97b6877d50262bd609fd371a8
AyushmanBhatt <103947002+AyushmanBhatt@users.noreply.github.com>
2022-04-18 20:45:09 +0530

    Create week4_p3_3kthsmalllarge.cpp

---
+++ week4_p3_3kthsmalllarge.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Name-Ayushman Bhatt 
+//Section-AI & DS 
+//University Roll No-2017640 
+//Class Roll No-21
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        int *arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            cin>>arr[i];
+        }
+        int k;
+        cin>>k;
+        priority_queue<int> pq;
+        for(int i = 0; i < k; i++) {
+            pq.push(arr[i]);
+        }
+        for(int i = k; i < n; i++) {
+            pq.push(arr[i]);
+            if(pq.size() > k) pq.pop();
+        }
+        if(pq.empty()) cout<<"Not present"<<endl;
+        else cout<<pq.top()<<endl;
+        delete []arr;
+    }
+}
