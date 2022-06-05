2e265bdd6c592c6acf066b1246c04152717daa9b
AyushmanBhatt <103947002+AyushmanBhatt@users.noreply.github.com>
2022-06-05 16:50:35 +0530

    Create week6_q2_bfs.cpp

---
+++ Week_6/week6_q2_bfs.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Name-Ayushman Bhatt 
+//Section-AI & DS 
+//University Roll No-2017640 
+//Class Roll No-21
+bool bfs(vector<int> adj[],int src, vector<int> &color,int n)
+{
+    queue<int> q;
+    q.push(src);
+    color[src]=1;
+    while (!q.empty())
+    {
+        int node=q.front();
+        q.pop();
+        for (auto it:adj[node])
+        {
+            if (color[it]==-1)
+            {
+                color[it]=1-color[node];
+                q.push(it);
+            }
+            else if (color[it]==color[node])
+               return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    cout<<"Enter the number of vertices and edges"<<endl;
+    int n,m;
+    cin>>n>>m;
+    vector<int> adj[n+1];
+    cout<<"enter the nodes between there is edge"<<endl;
+    for (int i=0;i<m;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    vector<int> color(n+1,-1);
+    if (bfs(adj,1,color,n))
+        cout<<"Yes Barpitite"<<endl;
+    else
+        cout<<"Not Barpitite"<<endl;
+    
+}
